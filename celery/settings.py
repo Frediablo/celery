4e78a51632ffbb01b061a227b1576fa4b77bd90e
Michał Tarasiewicz <michaltarasiewicz@MacBook-Pro.local>
2020-07-08 23:47:16 +0200

    new commit

--- celery/settings.py
+++ celery/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'celery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'celery',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
