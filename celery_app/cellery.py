1623964dc6b8716a5a16e7f235d59a1249d3f477
Michał Tarasiewicz <michaltarasiewicz@MacBook-Pro.local>
2020-07-09 11:55:38 +0200

    celery app rename

--- celery_app/cellery.py
+++ celery_app/cellery.py
@@ -4,14 +4,14 @@ import os
 
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery.settings')
+# set the default Django settings module for the 'celery_app' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_app.settings')
 
-app = Celery('celery')
+app = Celery('celery_app')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
+# - namespace='CELERY' means all celery_app-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
