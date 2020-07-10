f69de02028a5df52f315b2a4fa49a8c4320e46bc
Michał Tarasiewicz <michaltarasiewicz@MacBook-Pro.local>
2020-07-11 01:06:15 +0200

    adding table sorting culumns

--- websites/views.py
+++ websites/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, get_object_or_404
+from .models import Website, WebsiteCategory, WebPage
+# from .filters import WebsiteFilter
 from django.views.generic import ListView, DetailView, CreateView
 
+
 # Create your views here.
-
-
 class WebsiteListView(ListView):
-    queryset = Website.objects.all().order_by('id')
+    model = Website
     template_name = 'websites/websites.html'
-    paginate_by = 100
 
 
 class WebsiteDetailView(DetailView):
